6de7f25dbb040467b6d81af0edb163b59082b870
Imanuel Costigan <i.costigan@me.com>
2014-06-16 15:38:59 +1000

    Removed test of unexported function

--- tests/testthat/test-aemo.R
+++ tests/testthat/test-aemo.R
@@ -3,10 +3,6 @@ context ('URL')
 # URL scheme based on website on 15 Jun 2014
 # Some months' data uses the nemmco url e.g Nov 1999.
 
-test_that('AEMO regions complete', {
-  expect_equal(aemo_regions(), c('NSW', 'QLD', 'VIC', 'SA', 'TAS', 'SNOWY'))
-})
-
 test_that('URL stub is correct', {
   expect_equal(aemo_data_url_stub('nsw', 1999, 11),
     'http://www.nemmco.com.au/mms/data/DATA')
